cd360d0edddeb412a452ab6049d6b257b041ba96
Clement Verna <cverna@tutanota.com>
2025-10-03 19:22:42 +0200

    Initial import from https://github.com/cverna/laptop
    Signed-off-by: Clement Verna <cverna@tutanota.com>

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,15 +10,14 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y htop iwd libvirt-daemon libvirt-daemon libvirt-client libvirt-daemon-log \
+        libvirt-daemon-config-network libvirt-daemon-driver-interface libvirt-daemon-driver-network \
+        libvirt-daemon-driver-nwfilter libvirt-daemon-driver-qemu libvirt-daemon-driver-secret \
+        libvirt-daemon-driver-storage-core libvirt-dbus qemu-img qemu-kvm qemu-system-x86-core \
+        sysprof neovim ffmpeg-free gnome-tweaks gstreamer1-plugin-openh264 krb5-workstation \
+	simple-scan virt-manager crun genisoimage coreutils libselinux shadow-utils \
+        util-linux virtiofsd virt-install bootc just git jq podman
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
 
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+systemctl enable libvirtd.socket
+rm -rf /var/lib/unbound/root.key
